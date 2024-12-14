dbb9f336bb00485a49f535a35460f49b48926734
mariollesta <llesta.mario@gmail.com>
2024-12-14 12:19:25 +0100

    feat(frontend): basic project structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { App } from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import './index.css'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
     <App />
-  </StrictMode>,
 )
