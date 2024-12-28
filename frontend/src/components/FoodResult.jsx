85099349c181971cdc250474c03012f8d96209f5
mariollesta <llesta.mario@gmail.com>
2024-12-28 13:52:14 +0100

    feat(frontend): enhanced funcionality
    Responsive design.

--- frontend/src/components/FoodResult.jsx
+++ frontend/src/components/FoodResult.jsx
@@ -8,15 +8,14 @@ export function FoodResult({ name, rating, open_now }) {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row items-center p-4 bg-white rounded-lg shadow-md border-2 border-[#E0E0E0] hover:border-[#4CAF50] transition-all duration-300 transform hover:scale-105 overflow-hidden">
-      
+    <div className="flex flex-col md:flex-row items-start md:items-center justify-between p-4 bg-white rounded-lg shadow-md border-2 border-[#E0E0E0] hover:border-[#4CAF50] transition-all duration-300 transform hover:scale-105 overflow-hidden">
       {/* Restaurant details */}
-      <div className="flex-grow text-center sm:text-left mb-4 sm:mb-0">
+      <div className="flex-grow">
         <h3 className="text-lg font-bold text-[#FFA500] truncate">{name}</h3>
         {rating && (
-          <div className="flex items-center justify-center sm:justify-start mt-1 text-[#333333]">
+          <div className="flex items-center mt-1 text-[#333333]">
             <Star className="w-4 h-4 text-[#FFA500] mr-1" />
-            <span className="font-semibold text-[#333333]">{rating}</span>
+            <span className="font-semibold">{rating}</span>
 
             {/* Open/Closed Badge */}
             <span
@@ -35,7 +34,7 @@ export function FoodResult({ name, rating, open_now }) {
         href={generateGoogleMapsLink(name)}
         target="_blank"
         rel="noopener noreferrer"
-        className="w-full sm:w-36 h-10 flex items-center justify-center bg-[#FFFFFF] text-[#4CAF50] hover:bg-[#4CAF50] hover:text-[#FFFFFF] border border-[#4CAF50] rounded-md font-medium transition duration-300 ease-in-out"
+        className="mt-4 md:mt-0 md:ml-4 flex-shrink-0 flex items-center justify-center bg-[#FFFFFF] text-[#4CAF50] hover:bg-[#4CAF50] hover:text-[#FFFFFF] border border-[#4CAF50] rounded-md font-medium transition duration-300 ease-in-out px-4 py-2"
       >
         <MapPin className="w-4 h-4 mr-2" />
         Ver en Maps
