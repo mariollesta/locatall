7b713968b6eb9976500fbfd8c900c1387251165f
mariollesta <llesta.mario@gmail.com>
2025-01-12 13:04:43 +0100

    Release 0.1.0

--- api/main.py
+++ api/main.py
@@ -17,5 +17,4 @@ app.include_router(restaurants.router, prefix="/api", tags=["restaurants"])
 
 @app.get("/")
 def read_root():
-    google_api_key = os.getenv("GOOGLE_PLACES_API_KEY")
-    return {"message": f"¡Hola, mundo desde FastAPI! Tu API Key es {google_api_key}"}
+    return {"message": "Gotoeat"}
