cca659e6b7fd385886cba3155c39a4676e2c56e8
mariollesta <llesta.mario@gmail.com>
2025-01-18 18:24:03 +0100

    feat(): link to Github project

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Github } from 'lucide-react';
 
-const Footer = () => {
+const Footer = ({ githubLink }) => {
   return (
     <footer className="py-4 bg-[#333333] border-t border-[#4CAF50] w-full">
-      <div className="container mx-auto px-4 text-center text-[#FFFFFF] text-xs sm:text-sm font-semibold">
-          © {new Date().getFullYear()} gotoeat by mariollesta. All rights reserved.
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-center items-center text-center">
+        <div className="flex items-center text-[#FFFFFF] text-xs sm:text-sm font-semibold">
+          <span>© {new Date().getFullYear()} gotoeat. Proyecto open source.</span>
+          <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#FFFFFF] hover:text-[#4CAF50] transition-colors duration-200 flex items-center ml-2"
+            aria-label="View source on GitHub"
+          >
+            <Github className="w-4 h-4 sm:w-5 sm:h-5 lg:w-5 lg:h-5" />
+          </a>
+        </div>
       </div>
     </footer>
   );
