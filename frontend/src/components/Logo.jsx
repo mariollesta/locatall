d8d6eec2144ea74ca80371643ea7bd86c1a7bedd
mariollesta <llesta.mario@gmail.com>
2025-01-18 18:24:55 +0100

    feat(): immutable title language

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -5,10 +5,9 @@ const Logo = () => {
   return (
     <div className={"flex items-center"}>
       <UtensilsCrossed className="w-8 h-8" style={{ color: '#4CAF50' }} />
-      <span className="ml-2 text-xl font-semibold" style={{ color: '#FFA500' }}>gotoeat</span>
+      <span className="ml-2 text-xl font-semibold" style={{ color: '#FFA500' }} lang="en">gotoeat</span>
     </div>
   );
 };
 
 export default Logo;
-
