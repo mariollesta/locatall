a05ff59f6020f88fec6d2cbf4a0cf6c1870b8233
mariollesta <llesta.mario@gmail.com>
2025-01-26 16:47:41 +0100

    feat(): rebranding

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -14,15 +14,15 @@ export function ResultCard({ name, rating, open_now }) {
 
   return (
     <div
-      className={`bg-white bg-opacity-30 backdrop-blur-md shadow-md hover:shadow-lg transition-all duration-300 border border-white border-opacity-30 hover:scale-105`}
+      className={`bg-white bg-opacity-30 shadow-md hover:shadow-lg rounded-lg transition-all duration-300 border border-white border-opacity-30 hover:scale-105`}
     >
       <div className="p-4 flex items-center justify-between">
         {/* Restaurant info */}
         <div>
-          <h3 className="text-lg font-bold text-orange-500">{name}</h3>
+          <h3 className="text-lg font-bold text-[#3f2d85]">{name}</h3>
           {rating && (
-            <div className="flex items-center mt-1 text-gray-800">
-              <Star className="w-4 h-4 text-orange-500 mr-1" />
+            <div className="flex items-center mt-1 text-[#251b5a]">
+              <Star className="w-4 h-4 text-[#3f2d85] mr-1" />
               <span className="font-semibold mr-2">{rating}</span>
               <span
                 className={`inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-bold ${statusClasses}`}
@@ -38,7 +38,7 @@ export function ResultCard({ name, rating, open_now }) {
           href={generateGoogleMapsLink(name)}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center justify-center bg-white text-green-500 hover:bg-green-500 hover:text-white border border-green-500 rounded-lg font-medium transition duration-300 px-3 py-2 sm:px-4 text-sm sm:text-base"
+          className="flex items-center justify-center bg-[#e6e6fa] bg-opacity-30 text-[#3f2d85] hover:bg-[#dbdbf9]  border border-white border-opacity-30 shadow-md rounded-lg font-medium px-3 py-2 sm:px-4 text-sm sm:text-base"
           aria-label={`Abrir mapa de ${name}`}
         >
           <MapPin className="w-4 h-4 mr-2" />
