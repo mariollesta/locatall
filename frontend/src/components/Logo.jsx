688962f5efc7e8538bfd5400d40a21a8e33d5ca1
mariollesta <llesta.mario@gmail.com>
2025-01-29 16:37:34 +0100

    feat(): rebranding

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Compass } from 'lucide-react';
+import React from "react";
+import { Compass } from "lucide-react";
 
 const Logo = () => {
   return (
-    <div className={"flex items-center"}>
-      <Compass className="w-6 h-6" style={{ color: '#3f2d85' }} />
-      <span className="ml-2 text-xl font-semibold" style={{ color: '#3f2d85' }} lang="en">locatall</span>
+    <div className="flex items-center text-xl font-semibold" style={{ color: "#3f2d85" }}>
+      <span>l</span>
+      <Compass className="w-4 h-4" />
+      <span>catall</span>
     </div>
   );
 };
 
 export default Logo;
+
